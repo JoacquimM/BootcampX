054bb4262556c76a10dd65d919e287db8774d2f5
Joacquim Mukadi <joacquimm@gmail.com>
2021-04-30 00:54:29 +0000

    parameterized querries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,16 +8,73 @@ const pool = new Pool({
 });
 
 // queries to students table
+// pool
+//   .query(
+//     `
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `
+//   )
+//   .then((res) => {
+//     // console.log(res);
+//     console.log(res.rows);
+//   })
+//   .catch((err) => console.error("query error", err.stack));
+
+//----------------------------------------------------------------
+//the above but with a join clause
+
+//   pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// });
+//----------------------------------------------------------------
+// same as both above but allows user to enter soecified cogort date and limit to
+
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%' /*FEB*/
+// LIMIT ${process.argv[3] || 5};  /*3 or 5*/
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
+
+// ----------------------------------------------------------------
+
+// use parameterized queries
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 pool
-  .query(
-    `
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5;
-`
-  )
+  .query(queryString, values)
   .then((res) => {
-    // console.log(res);
-    console.log(res.rows);
+    // console.log("RES-->", res);
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
   })
   .catch((err) => console.error("query error", err.stack));
